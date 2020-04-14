c02b2feb153fcbd9bc7eb6deaa74ac88b93e1c41
Andressa Silva <andressa.silva.estagiario@tre-rn.jus.br>
2020-04-14 14:15:20 -0300

    Final program

--- src/main/java/application/Program19.java
+++ src/main/java/application/Program19.java
@@ -1,10 +1,11 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Ler uma matriz com n elementos e informar quantos numeros negativos tem e
+ * qual a diagonal principal.
  */
+
 package application;
 
+import java.util.Locale;
 import java.util.Scanner;
 
 /**
@@ -15,29 +16,44 @@ public class Program19 {
     
     public static void main(String[] args){
         
+        Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
         
+        System.out.print("Enter matrix order: ");
         int n = sc.nextInt();
-        int[][] mat = new int[n][n];
         
+        int [][] mat = new int[n][n];
+        System.out.println();
+        
+        int countNeg = 0;
         for(int i=0; i<mat.length; i++){
             for(int j=0; j<mat[i].length; j++){
-                mat[i][j] = sc.nextInt();
+             System.out.printf("Enter element line %d column %d: ", i, j);
+             mat[i][j] = sc.nextInt();
+             
+             if(mat[i][j] < 0){
+                 countNeg++;
+             }
             }
+            System.out.println();
         }
-        System.out.println("Main diagonal ");
+        System.out.println("Matrix: ");
         for(int i=0; i<mat.length; i++){
-            System.out.println(mat[i][i] + " ");
+            for(int j=0; j<mat[i].length; j++){
+                System.out.print(mat[i][j] + " ");
+            }
+            System.out.println();
         }
         System.out.println();
-        int count =0;
+        System.out.println("Main diagonal: ");
         for(int i=0; i<mat.length; i++){
             for(int j=0; j<mat[i].length; j++){
-                if(mat[i][j]  < 0){
-                    count ++;
+                if(mat[i] == mat[j]){
+                    System.out.println(mat[i][j]);
                 }
             }
         }
-        System.out.print("Negative numbers: " +count);
+        System.out.printf("%n%n");
+        System.out.println("Negative numbers: " +countNeg);
     }
 }
